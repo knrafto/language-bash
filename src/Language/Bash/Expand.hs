72742f1f70ce2dcf767bef992f748d9d2fb3d452
Kyle Raftogianis <kylerafto@gmail.com>
2014-01-16 23:20:57 -0800

    Brace expand words

--- src/Language/Bash/Expand.hs
+++ src/Language/Bash/Expand.hs
@@ -1,50 +1,53 @@
-{-# LANGUAGE PatternGuards #-}
--- | Bash expansions.
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+-- | Shell expansions.
 module Language.Bash.Expand
     ( braceExpand
-    , TildePrefix(..)
-    , tildePrefix
-    , splitWord
-    , unquote
     ) where
 
-import           Control.Applicative
-import           Control.Monad
-import           Data.Char
-import           Data.List
-import           Data.Monoid
-import           Data.Traversable
-import           Text.Parsec                  hiding ((<|>), optional)
+import Control.Applicative
+import Control.Monad
+import Data.Char
+import Data.Traversable
+import Text.Parsec.Combinator hiding (optional, manyTill)
+import Text.Parsec.Prim       hiding ((<|>), many, token)
+import Text.Parsec.String     ()
 
-import qualified Language.Bash.Parse.Builder  as B
-import           Language.Bash.Parse.Internal
-import           Language.Bash.Syntax
+import Language.Bash.Pretty
+import Language.Bash.Word
 
--- | @upTo n p@ parses zero to @n@ occurences of @p@.
-upTo :: Alternative f => Int -> f a -> f [a]
-upTo m p = go m
-  where
-    go n | n < 0     = empty
-         | n == 0    = pure []
-         | otherwise = (:) <$> p <*> go (n - 1) <|> pure []
+-- | A parser over words.
+type Parser = Parsec Word ()
 
--- | @upTo1 n p@ parses one to @n@ occurences of @p@.
-upTo1 :: Alternative f => Int -> f a -> f [a]
-upTo1 n p = (:) <$> p <*> upTo (n - 1) p
+-- | Run a 'Parser', failing on a parse error.
+parseUnsafe :: String -> Parser a -> Word -> a
+parseUnsafe f p w = case parse p (prettyText w) w of
+    Left  e -> error $ "Language.Bash.Expand." ++ f ++ ": " ++ show e
+    Right a -> a
 
--- | Parse a word with a parser that should always succeed.
-parseWord :: String -> Parsec String () a -> String -> a
-parseWord fname p s = case parse p s s of
-    Left  e -> error $ "Language.Bash.Expand." ++ fname ++ ": " ++ show e
-    Right r -> r
+-- | Parse a general token.
+token :: (Span -> Maybe a) -> Parser a
+token = tokenPrim (const "") (\pos _ _ -> pos)
 
--- | Pad a number to a specified width
-showPadded :: Int -> Int -> String
-showPadded w n
-    | n < 0     = '-' : showPadded (w - 1) (negate n)
-    | otherwise = replicate (w - length s) '0' ++ s
-  where
-    s = show n
+-- | Parse an unquoted character satisfying a predicate.
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = token $ \case
+    Char c | p c -> Just c
+    _            -> Nothing
+
+-- | Parse a word until an unquoted character that satisfies the predicate
+-- appears.
+manyTill :: (Char -> Bool) -> Parser Word
+manyTill p = many $ token $ \case
+    Char c | p c -> Nothing
+    s            -> Just s
+
+-- | Parse an unquoted character.
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+-- | Parse an unquoted string.
+string :: String -> Parser String
+string = traverse char
 
 -- | Read a number.
 fromNumber :: MonadPlus m => String -> m Int
@@ -53,10 +56,10 @@ fromNumber s = case s of
     _      -> readNumber s
   where
     readNumber t = case reads t of
-        [(n,"")] -> return n
-        _        -> mzero
+        [(n, "")] -> return n
+        _         -> mzero
 
--- | Read a character.
+-- | Read a letter.
 fromAlpha :: MonadPlus m => String -> m Char
 fromAlpha [c] | isAlpha c = return c
 fromAlpha _               = mzero
@@ -70,142 +73,56 @@ enum x y inc = map toEnum [fromEnum x, fromEnum x + step .. fromEnum y]
                 | otherwise -> 1
         Just i              -> i
 
--- | Brace expand a word, including sequences.
+-- | Brace expand a word.
 braceExpand :: Word -> [Word]
-braceExpand = parseWord "braceExpand" (map B.toString <$> brace "")
+braceExpand = parseUnsafe "braceExpand" (go "")
   where
-    brace delims = try (expansion delims)
-               <|> return <$> gobble delims
+    go delims = try (brace delims)
+            <|> (:[]) <$> manyTill (`elem` delims)
 
-    expansion delims = do
-        a  <- gobble ('{':delims)
+    brace delims = do
+        a  <- manyTill (`elem` ('{' : delims))
         _  <- char '{'
-        bs <- try sequenceExpansion <|> braceParts
+        bs <- try sequenceExpand <|> braceParts
         _  <- char '}'
-        cs <- brace delims
-        return [a <> b <> c | b <- bs, c <- cs]
+        cs <- go delims
+        return [ a ++ b ++ c | b <- bs, c <- cs ]
 
-    braceParts = concatParts <$> brace ",}" `sepBy` char ','
+    braceParts = concatParts <$> go ",}" `sepBy` char ','
 
-    concatParts []   = [B.fromString "{}"]
-    concatParts [xs] = map (\x -> B.fromChar '{' <> x <> B.fromChar '}') xs
+    concatParts []   = ["{}"]
+    concatParts [xs] = map (\x -> "{" ++ x ++ "}") xs
     concatParts xss  = concat xss
- 
-    sequenceExpansion = do
+
+    sequenceExpand = do
         a   <- sequencePart
         b   <- string ".." *> sequencePart
         c   <- optional (string ".." *> sequencePart)
         inc <- traverse fromNumber c
-        charExpansion a b inc <|> numberExpansion a b inc
+        map fromString <$> (numExpand a b inc <|> charExpand a b inc)
       where
-        sequencePart = many1 (satisfy (\c -> isAlphaNum c || c == '-'))
- 
-    charExpansion a b inc = do
+        sequencePart = many1 (satisfy isAlphaNum)
+
+    charExpand a b inc = do
         x <- fromAlpha a
         y <- fromAlpha b
-        return . map B.fromChar $ enum x y inc
- 
-    numberExpansion a b inc = do
+        return . map (:[]) $ enum x y inc
+
+    numExpand a b inc = do
         x <- fromNumber a
         y <- fromNumber b
-        return . map (B.fromString . render) $ enum x y inc
+        return . map showPadded $ enum x y inc
       where
         width = max (length a) (length b)
 
         isPadded ('-':'0':_:_) = True
         isPadded ('0':_:_)     = True
         isPadded _             = False
- 
-        render = if isPadded a || isPadded b
-                 then showPadded width
-                 else show
 
--- | A Bash tilde prefix of a word.
-data TildePrefix
-    = Home             -- ^ '~/foo'
-    | UserHome String  -- ^ '~fred/foo'
-    | PWD              -- ^ '~+/foo'
-    | OldPWD           -- ^ '~-/foo'
-    | Dirs Int         -- ^ '~N', '~+N', '~-N'
-    deriving (Eq, Read, Show)
+        showPadded = if isPadded a || isPadded b then pad width else show
 
--- | Get the tilde prefix of a word, if there is one. The remaining word
--- may be empty.
-tildePrefix :: Word -> (Maybe TildePrefix, Word)
-tildePrefix w = case parse tilde "" w of
-    Left _        -> (Nothing, w)
-    Right (p, w') -> (Just p, w')
-  where
-    tilde = do
-        _ <- char '~'
-        s <- B.toString <$> gobble "/"
-        r <- getInput
-        return (readTilde s, r)
-
-    readTilde s
-        | s == ""                = Home
-        | s == "+"               = PWD
-        | s == "-"               = OldPWD
-        | Just n <- fromNumber s = Dirs n
-        | otherwise              = UserHome s
-
--- | Split a word into parts based on a the specified delimiters.
-splitWord :: [Char] -> Word -> [Word]
-splitWord ifs = parseWord "splitWord" (map B.toString <$> go)
-  where
-    go = do
-        skipMany (oneOf ifs)
-        [] <$ eof <|> (:) <$> gobble ifs <*> go
-
--- | Unquote a word.
-unquote :: Word -> String
-unquote s = case parse unquoteBare s s of
-    Left _   -> s
-    Right s' -> B.toString s'
-  where
-    unquoteBare = B.many $
-            try unquoteEscape
-        <|> try unquoteSingle
-        <|> try unquoteDouble
-        <|> try unquoteAnsi
-        <|> try unquoteLocale
-        <|> B.anyChar
-
-    unquoteEscape = char '\\' *> B.anyChar
-    unquoteSingle = B.span '\'' '\'' empty
-    unquoteDouble = B.span '\"' '\"' unquoteEscape
-    unquoteAnsi   = char '$' *> B.span '\'' '\'' (try ansiEscape)
-    unquoteLocale = char '$' *> unquoteDouble
-
-    ansiEscape = B.fromChar <$ char '\\' <*> escapeCode
-
-    escapeCode = charCodes
-             <|> char 'x' *> hex 2
-             <|> char 'u' *> hex 4
-             <|> char 'U' *> hex 8
-             <|> oct 3
-             <|> char 'c' *> ctrlCodes
-
-    charCodes = codes "abeEfnrtv\\\'\"" "\a\b\ESC\ESC\f\n\r\t\v\\\'\""
-
-    ctrlCodes = '\FS' <$ try (string "\\\\")
-            <|> codes "@ABCDEFGHIJKLMOPQRSTUVWXYZ[]^_?"
-                      ("\NUL\SOH\STX\ETX\EOT\ENQ\ACK\BEL\BS\HT\LF\VT\FF" ++
-                       "\CR\SO\SI\DLE\DC1\DC2\DC3\DC4\NAK\SYN\ETB\CAN\EM" ++
-                       "\SUB\ESC\GS\RS\US\DEL")
-
-    codes chars replacements = do
-        c <- anyChar
-        case lookup c table of
-            Nothing -> unexpected [c]
-            Just c' -> return c'
-      where
-        table = zip chars replacements
-
-    oct n = number n 8 octDigit
-    hex n = number n 16 hexDigit
-
-    number maxDigits base baseDigit = do
-        digits <- map digitToInt <$> upTo1 maxDigits baseDigit
-        let n = foldl' (\x d -> base*x + d) 0 digits
-        return $ if n > ord maxBound then '\0' else chr n  -- arbitrary
+        pad w n
+            | n < 0     = '-' : pad (w - 1) (negate n)
+            | otherwise = replicate (w - length s) '0' ++ s
+          where
+            s = show n
