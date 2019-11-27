696c622306fe937a6771e0fe0938199a51e1a0a5
Mann mit Hut <mmh@riseup.net>
2019-11-27 02:07:18 +0100

    Added more documentation to the Pretty module

--- src/Language/Bash/Pretty.hs
+++ src/Language/Bash/Pretty.hs
@@ -9,14 +9,31 @@ import Data.Text.Prettyprint.Doc.Render.String
 instance (Pretty a, Pretty b) => Pretty (Either a b) where
     pretty = either pretty pretty
 
+-- | @x $+$ y@ concatenates @x@ and @y@ with a 'line' in between
 ($+$) :: Doc ann -> Doc ann -> Doc ann
 x $+$ y = x <> line <> y
 
+-- | Behaves like '$+$' except that if one of the documents was empty we do not concatenate at all.
+-- 'mempty' is the identity of '$+$':
+--
+-- prop> x $++$ mempty == x
+--
+-- and
+--
+-- prop> mempty $++$ y == y
 ($++$) :: Doc ann -> Doc ann -> Doc ann
 Empty $++$ y     = y
 x     $++$ Empty = x
 x     $++$ y     = x <> line <> y
 
+-- | Behaves like '<+>' except that if one of the documents was empty we do not concatenate at all.
+-- 'mempty' is the identity of '<+>':
+--
+-- prop> x <++> mempty == x
+--
+-- and
+--
+-- prop> mempty <++> y == y
 (<++>) :: Doc ann -> Doc ann -> Doc ann
 Empty <++> y     = y
 x     <++> Empty = x
